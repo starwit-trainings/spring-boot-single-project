acadbb2003baf9125b39325144ca250e37a903a9
flonix8 <flstanek@googlemail.com>
2023-09-26 14:47:09 +0200

    Add solution for exercise 1

---
+++ webshop/src/main/java/training/springboot/webshop/Greeter.java
@@ -0,0 +1,22 @@
+package training.springboot.webshop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeter implements CommandLineRunner {
+
+    private static Logger log = LoggerFactory.getLogger(Greeter.class);
+
+    @Value("${greeting.text}")
+    private String greetingText;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Hello " + this.greetingText);
+    }
+    
+}
