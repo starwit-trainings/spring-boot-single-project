a761a0f73542ea4392ad1a5dca5120cbc99cf892
flonix8 <flstanek@googlemail.com>
2023-09-26 17:41:53 +0200

    Add base for exercise 3

---
+++ webshop/src/main/java/training/springboot/webshop/entity/Product.java
@@ -0,0 +1,47 @@
+package training.springboot.webshop.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private long id;
+    private String name;
+    private BigDecimal price;
+    private String description;
+
+    public Product(long id, String name, BigDecimal price, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    // no setter because we don't want to accidentally change the identity of an entity
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
