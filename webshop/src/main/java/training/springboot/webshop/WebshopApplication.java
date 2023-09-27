4f140e68016db0650d82ed3a4d8f8e6911c8b4fe
flonix8 <flstanek@googlemail.com>
2023-09-27 15:55:20 +0200

    Add solution for exercise 5

--- webshop/src/main/java/training/springboot/webshop/WebshopApplication.java
+++ webshop/src/main/java/training/springboot/webshop/WebshopApplication.java
@@ -2,8 +2,10 @@ package training.springboot.webshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class WebshopApplication {
 
 	public static void main(String[] args) {
