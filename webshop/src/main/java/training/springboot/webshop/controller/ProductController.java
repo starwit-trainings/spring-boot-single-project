288cffc167f8bb62ddb6db31da8d46c93b065b8c
flonix8 <flstanek@googlemail.com>
2023-09-27 10:25:41 +0200

    Add ProductCatalog to exercise 3 base (make it easier)

--- webshop/src/main/java/training/springboot/webshop/controller/ProductController.java
+++ webshop/src/main/java/training/springboot/webshop/controller/ProductController.java
@@ -2,6 +2,8 @@ package training.springboot.webshop.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +16,8 @@ import training.springboot.webshop.entity.Product;
 public class ProductController {
     
     @GetMapping("/{id}")
-    public Product get(@PathVariable long id) {
-        return new Product(id, "test", new BigDecimal("9.99"), "This is a dummy product");
+    public ResponseEntity<Product> get(@PathVariable long id) {
+        Product dummyProduct = new Product(id, "test", new BigDecimal("9.99"), "This is a dummy product");
+        return new ResponseEntity<Product>(dummyProduct, HttpStatus.OK);
     }
 }
