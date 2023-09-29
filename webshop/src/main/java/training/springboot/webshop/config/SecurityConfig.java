d56e9f5ab9975b4691eb2378036b01a962c9d0f1
flonix8 <flstanek@googlemail.com>
2023-09-29 18:06:40 +0200

    Add simple authentication and authorization setup with Spring Security

--- webshop/src/main/java/training/springboot/webshop/config/SecurityConfig.java
+++ webshop/src/main/java/training/springboot/webshop/config/SecurityConfig.java
@@ -1,20 +1,50 @@
 package training.springboot.webshop.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@EnableWebSecurity
+@Component
 public class SecurityConfig {
     
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // We will disable CSRF protection for now (as we do not have a stateful client)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.GET).hasRole("USER")
+                .requestMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE).hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+            )
+            .httpBasic(Customizer.withDefaults());
+
+
         http.csrf(csrf -> csrf.disable());
-        
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        // The builder will ensure the passwords are encoded before saving in memory
+        UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users
+            .username("customuser")
+            .password("password")
+            .roles("USER")
+            .build();
+        UserDetails admin = users
+            .username("admin")
+            .password("password")
+            .roles("USER", "ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
