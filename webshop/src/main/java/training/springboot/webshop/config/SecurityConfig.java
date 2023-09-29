83d824383026d34ca086a43404bf3eb88d4e433a
flonix8 <flstanek@googlemail.com>
2023-09-29 10:37:44 +0200

    Add preliminary solution for ex 7

---
+++ webshop/src/main/java/training/springboot/webshop/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package training.springboot.webshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // We will disable CSRF protection for now (as we do not have a stateful client)
+        http.csrf(csrf -> csrf.disable());
+        
+        return http.build();
+    }
+}
