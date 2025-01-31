affd5c9d23ea32ba5167e8268b001856bb922c9b
flonix8 <flstanek@googlemail.com>
2025-01-31 12:37:54 +0100

    Fix 409 on POST

--- webshop/src/main/java/training/springboot/webshop/entity/Product.java
+++ webshop/src/main/java/training/springboot/webshop/entity/Product.java
@@ -3,23 +3,23 @@ package training.springboot.webshop.entity;
 import java.math.BigDecimal;
 
 public class Product {
-    private long id;
+    private Long id;
     private String name;
     private BigDecimal price;
     private String description;
 
-    public Product(long id, String name, BigDecimal price, String description) {
+    public Product(Long id, String name, BigDecimal price, String description) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
